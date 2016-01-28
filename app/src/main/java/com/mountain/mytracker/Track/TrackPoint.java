46a2d479f9d45439079ef53fee08b645bd02a2e1
Andrei <andreistirb@yahoo.com>
2016-01-28 17:49:19 +0200

    add trackpoint fields

--- app/src/main/java/com/mountain/mytracker/Track/TrackPoint.java
+++ app/src/main/java/com/mountain/mytracker/Track/TrackPoint.java
@@ -3,5 +3,12 @@ package com.mountain.mytracker.Track;
 /**
  * Created by astirb on 26.01.2016.
  */
+
 public class TrackPoint {
+
+    private int trackId;
+    private Double altitude, latitude, longitude;
+    private Float speed, accuracy;
+    private Long time;
+
 }
