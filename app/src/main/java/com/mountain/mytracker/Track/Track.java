49009a88630d3197f8a4a763f688479503fffaaf
Andrei <andreistirb@yahoo.com>
2016-01-29 17:56:10 +0200

    add database handling prototypes

--- app/src/main/java/com/mountain/mytracker/Track/Track.java
+++ app/src/main/java/com/mountain/mytracker/Track/Track.java
@@ -1,9 +1,20 @@
 package com.mountain.mytracker.Track;
 
+import java.util.ArrayList;
+
 /**
  * Created by astirb on 26.01.2016.
  */
 public class Track {
+    private int trackId, mountainId;
+    private ArrayList<TrackPoint> trackPoints;
 
 
+    public void fromDatabase(){
+
+    }
+
+    public void toDatabase(){
+
+    }
 }
--- app/src/main/java/com/mountain/mytracker/Track/TrackPoint.java
+++ app/src/main/java/com/mountain/mytracker/Track/TrackPoint.java
@@ -11,4 +11,21 @@ public class TrackPoint {
     private Float speed, accuracy;
     private Long time;
 
+    public TrackPoint(int trackId, Double latitude, Double longitude, Double altitude, Float speed, Float accuracy, Long time){
+        this.trackId = trackId;
+        this.altitude = altitude;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.speed = speed;
+        this.accuracy = accuracy;
+        this.time = time;
+    }
+
+    public void fromDatabase(){
+
+    }
+
+    public void toDatabase(){
+
+    }
 }
