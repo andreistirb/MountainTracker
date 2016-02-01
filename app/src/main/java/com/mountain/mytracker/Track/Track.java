2ce5d7af4d1c0239389ac4667b7e65a8807a7402
Andrei Stirb <andreistirb@yahoo.com>
2016-02-01 08:40:22 +0200

    add factory database method

--- app/src/main/java/com/mountain/mytracker/Track/Track.java
+++ app/src/main/java/com/mountain/mytracker/Track/Track.java
@@ -1,18 +1,72 @@
 package com.mountain.mytracker.Track;
 
+import android.content.Context;
+import android.database.Cursor;
+
+import com.mountain.mytracker.db.DatabaseContract.DatabaseEntry;
+import com.mountain.mytracker.db.DatabaseHelper;
+import com.mountain.mytracker.db.NewDatabaseHelper;
+
+import java.io.Serializable;
 import java.util.ArrayList;
 
 /**
  * Created by astirb on 26.01.2016.
  */
-public class Track {
-    private int trackId, mountainId;
+
+public class Track implements Serializable {
+    private Integer trackId, mountainId;
+    private String trackName, trackDifficulty, trackMark, trackLength, trackDescription, trackAvailability;
     private ArrayList<TrackPoint> trackPoints;
 
+    //Database
+    private DatabaseHelper mDatabase;
+    private NewDatabaseHelper factoryDatabase;
 
-    public void fromDatabase(){
-
+    public Track(Integer trackId){
+        this.trackId = trackId;
     }
+
+    public void addTrackPoint(TrackPoint trackPoint){
+        this.trackPoints.add(trackPoint);
+    }
+
+    public Track fromFactoryDatabase(Integer trackId, Context context){
+        Track newTrack = new Track(trackId);
+        TrackPoint newTrackPoint;
+
+        //We also need data about track, not only trackpoints!!!
+        String selection = DatabaseEntry.COL_TRACK_ID + " = ? ";
+        String[] selectionArgs = new String[] { trackId.toString() };
+        String table = DatabaseEntry.TABLE_TRACK_POINTS;
+        String sortOrder = DatabaseEntry.COL_ORD;
+
+        Cursor c = factoryDatabase.myQuery(table, null, selection, selectionArgs, null,
+                null, sortOrder);
+
+        c.moveToFirst();
+
+        do {
+            Double latitude = c.getDouble(c.getColumnIndex(DatabaseEntry.COL_LAT));
+            Double longitude = c.getDouble(c.getColumnIndex(DatabaseEntry.COL_LON));
+            Double altitude = c.getDouble(c.getColumnIndex(DatabaseEntry.COL_ALT));
+            //Must modify database to contain this columns!!!
+            //Float speed = c.getFloat(c.getColumnIndex(DatabaseEntry.COL_SPD));
+            //Float accuracy = c.getFloat(c.getColumnIndex(DatabaseEntry.COL_ACC));
+            //Long time = c.getLong(c.getColumnIndex(DatabaseEntry.COL_TMP));
+
+            newTrackPoint = new TrackPoint(trackId, latitude, longitude, altitude, null, null, null, context);
+            newTrack.addTrackPoint(newTrackPoint);
+
+        } while (c.moveToNext());
+
+        return newTrack;
+    }
+
+    /*public Track fromDatabase(){
+        Track newPoint;
+        return newPoint;
+    }*/
 
     public void toDatabase(){
 
--- app/src/main/java/com/mountain/mytracker/Track/TrackPoint.java
+++ app/src/main/java/com/mountain/mytracker/Track/TrackPoint.java
@@ -1,17 +1,33 @@
 package com.mountain.mytracker.Track;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.util.Log;
+
+import com.mountain.mytracker.db.DatabaseContract.DatabaseEntry;
+import com.mountain.mytracker.db.DatabaseHelper;
+import com.mountain.mytracker.db.NewDatabaseHelper;
+
 /**
  * Created by astirb on 26.01.2016.
  */
 
 public class TrackPoint {
 
-    private int trackId;
+    //Attributes
+    private Integer trackId;
     private Double altitude, latitude, longitude;
     private Float speed, accuracy;
     private Long time;
 
-    public TrackPoint(int trackId, Double latitude, Double longitude, Double altitude, Float speed, Float accuracy, Long time){
+    //Database
+    private DatabaseHelper mDatabase;
+    private NewDatabaseHelper factoryDatabase;
+
+
+    public TrackPoint(int trackId, Double latitude, Double longitude, Double altitude,
+                      Float speed, Float accuracy, Long time, Context context){
         this.trackId = trackId;
         this.altitude = altitude;
         this.latitude = latitude;
@@ -19,13 +35,23 @@ public class TrackPoint {
         this.speed = speed;
         this.accuracy = accuracy;
         this.time = time;
-    }
 
-    public void fromDatabase(){
-
+        this.mDatabase = new DatabaseHelper(context);
+        this.factoryDatabase = new NewDatabaseHelper(context);
     }
 
     public void toDatabase(){
+        ContentValues row = new ContentValues();
 
+        row.put(DatabaseEntry.COL_ALT, altitude);
+        row.put(DatabaseEntry.COL_LAT, latitude);
+        row.put(DatabaseEntry.COL_LON, longitude);
+        //Must modify database to contain this columns
+        //row.put(DatabaseEntry.COL_ACC, accuracy);
+        //row.put(DatabaseEntry.COL_SPD, speed);
+        //row.put(DatabaseEntry.COL_TMP, time);
+        row.put(DatabaseEntry.COL_TRACK_NO, trackId.toString());
+        mDatabase.getWritableDatabase().insert(DatabaseEntry.TABLE_MY_TRACKS_POINTS, null, row);
+        mDatabase.close();
     }
 }
