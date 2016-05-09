3bb362fc2ccebd31b146840a5b22a08eb2ff3e68
Andrei <andreistirb@yahoo.com>
2016-05-09 13:37:30 +0300

    add navigation drawer and bind to settings activity

--- app/src/main/java/com/mountain/mytracker/activity/MainActivity.java
+++ app/src/main/java/com/mountain/mytracker/activity/MainActivity.java
@@ -4,21 +4,39 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.mountain.mytracker.other.NavigationDrawerItemClickListener;
 import com.mountain.mytracker.other.SettingsActivity;
 
 public class MainActivity extends Activity {
 
-	Button about_btn, my_tracks, mountain_list;
+	private Button about_btn, my_tracks, mountain_list;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private String[] mNavigationDrawerItems;
 
     @Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.main_layout);
+
+        mNavigationDrawerItems = getResources().getStringArray(R.array.navigation_drawer_items);
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.navigation_drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.navigation_drawer_list);
+
+        //Set the adapter for the list view and list's click listener
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.navigation_drawer_item, mNavigationDrawerItems));
+        mDrawerList.setOnItemClickListener(new NavigationDrawerItemClickListener());
+
 
 		mountain_list = (Button) this.findViewById(R.id.main_mountain_btn);		
 		mountain_list.setOnClickListener(new View.OnClickListener() {
