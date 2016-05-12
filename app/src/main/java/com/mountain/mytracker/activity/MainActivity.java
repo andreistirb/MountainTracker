df71b31465ee0fa79364242e1f713e7bab58acd6
Andrei <andreistirb@yahoo.com>
2016-05-12 13:23:39 +0300

    functional navigation drawer with settings page. need to design it

--- app/src/main/java/com/mountain/mytracker/activity/MainActivity.java
+++ app/src/main/java/com/mountain/mytracker/activity/MainActivity.java
@@ -2,19 +2,19 @@ package com.mountain.mytracker.activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.mountain.mytracker.other.NavigationDrawerItemClickListener;
-import com.mountain.mytracker.other.SettingsActivity;
+
 
 public class MainActivity extends Activity {
 
@@ -22,6 +22,7 @@ public class MainActivity extends Activity {
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
     private String[] mNavigationDrawerItems;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
 	public void onCreate(Bundle savedInstanceState){
@@ -36,6 +37,31 @@ public class MainActivity extends Activity {
         mDrawerList.setAdapter(new ArrayAdapter<String>(this,
                 R.layout.navigation_drawer_item, mNavigationDrawerItems));
         mDrawerList.setOnItemClickListener(new NavigationDrawerItemClickListener());
+
+        //set the actionBarDrawerToggle so that user can open NavigationDrawer from appIcon
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                mDrawerLayout,
+                R.drawable.ic_launcher,
+                R.string.navigation_drawer_open,
+                R.string.app_name
+        ){
+            public void onDrawerClosed(View v){
+                super.onDrawerClosed(v);
+                getActionBar().setTitle(R.string.app_name);
+            }
+
+            public void onDrawerOpened(View v){
+                super.onDrawerOpened(v);
+                getActionBar().setTitle(R.string.navigation_drawer_open);
+            }
+        };
+
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        //getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
 
 
 		mountain_list = (Button) this.findViewById(R.id.main_mountain_btn);		
@@ -69,15 +95,27 @@ public class MainActivity extends Activity {
 			}
 		});
 
-        Button settings_btn = (Button) this.findViewById(R.id.main_settings_btn);
-        settings_btn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent i = new Intent(v.getContext(), SettingsActivity.class);
-                startActivity(i);
-            }
-        });
-
-
 	}
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(mDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /* TO-DO see if this is a real thing and if it can boost somehow the app */
+    /*@Override
+    protected void onPostCreate(Bundle savedInstanceState){
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }*/
+
+    /*@Override
+    public void onConfigurationChanged(Configuration newConfig){
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }*/
 }
